b7b2a5852ffc801851cc57dd3e04153468a4dd9f
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2022-04-12 09:14:51 +0530

    #5 show ipv4 address show ipv4 address of device after starting vnc

--- utils/info.py
+++ utils/info.py
@@ -1,14 +1,15 @@
-import os
+from ipaddress import IPv4Address
+import os, socket
 from utils.funs import *
 
 ######## VNC
-HOSTPUBIP=os.system("hostname -I | cut -d ' ' -f 2")
+IPv4Address=socket.gethostbyname(socket.gethostname())
 LOCALIP="127.0.0.1"
 
 def vncokdialog(port):
     return "vncserver started on "\
         +"\n"+magneta(str(LOCALIP)+":"+str(port))\
-        +"\n"+magneta(str(LOCALIP)+":"+str(port))\
+        +"\n"+magneta(str(IPv4Address)+":"+str(port))\
         +"\n"+"To stop it, run:\n"\
         +green("vncserver -kill :"+str(port))\
         +" or "+blue("stopvnc")
@@ -16,7 +17,7 @@ def vncokdialog(port):
 def vncrunningdialog(port):
     return "vncserver already started on "\
         +"\n"+magneta(str(LOCALIP)+":"+str(port))\
-        +"\n"+magneta(str(LOCALIP)+":"+str(port))\
+        +"\n"+magneta(str(IPv4Address)+":"+str(port))\
         +"\n"+"To stop it, run:\n"\
         +green("vncserver -kill :"+str(port))\
         +" or "+blue("stopvnc")
