05d04da1cd2a2473e94a393758820b22f8aae6de
DARK <80549753+Ayon-SSP@users.noreply.github.com>
2022-06-09 15:43:33 +0530

    Bug FIx #11
    It will update last_modified_time in every iteration[changed in .txt file]

--- utils/clipboard_watcher.py
+++ utils/clipboard_watcher.py
@@ -18,6 +18,8 @@ def start_clipboard_service():
         if os.path.exists(CLIPBOARD_BUFFER_FILE):
             if os.path.getmtime(CLIPBOARD_BUFFER_FILE) != last_modified_time:
                 os.system(f"cat {CLIPBOARD_BUFFER_FILE} | xclip -selection clipboard")
+                last_modified_time = os.path.getmtime(CLIPBOARD_BUFFER_FILE)
         else:
             print("clipboard buffer file not found")
             os.exit(1)
+        
