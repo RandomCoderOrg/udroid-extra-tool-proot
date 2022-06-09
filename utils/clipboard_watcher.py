1a4e2e26e071fec500a1d59d305fd92d8fd53772
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2022-06-09 16:52:41 +0530

    reformat

--- utils/clipboard_watcher.py
+++ utils/clipboard_watcher.py
@@ -1,27 +1,30 @@
-import os, time
+import os
+import time
 
-CLIPBOARD_BUFFER_FILE="/tmp/clipboard"
+CLIPBOARD_BUFFER_FILE = "/tmp/clipboard"
+
 
 def start_clipboard_service():
     """
     watch if clipboard buffer file has changed
     if true, then pipe content buffer to xclip
     """
-    buffer=os.popen(f"cat {CLIPBOARD_BUFFER_FILE}").read().strip()
-    
+    buffer = os.popen(f"cat {CLIPBOARD_BUFFER_FILE}").read().strip()
+
     # check is xclip installed
     _xclip = os.popen("which xclip").read().strip()
     if _xclip is None:
         print("xclip not found")
         os.exit(1)
-    
+
     while os.getenv('DISPLAY') is not None:
         time.sleep(0.4)
         if os.path.exists(CLIPBOARD_BUFFER_FILE):
-            if buffer!=os.popen(f"cat {CLIPBOARD_BUFFER_FILE}").read().strip():
-                os.system(f"cat {CLIPBOARD_BUFFER_FILE} | xclip -selection clipboard")
-                buffer=os.popen(f"cat {CLIPBOARD_BUFFER_FILE}").read().strip()
+            if buffer != os.popen(f"cat {CLIPBOARD_BUFFER_FILE}").read().strip():
+                os.system(
+                    f"cat {CLIPBOARD_BUFFER_FILE} | xclip -selection clipboard")
+                buffer = os.popen(
+                    f"cat {CLIPBOARD_BUFFER_FILE}").read().strip()
         else:
             print("clipboard buffer file not found")
             os.exit(1)
-        
